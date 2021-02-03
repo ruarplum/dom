10fb372f6a35cc35848f99e488f7abf711398dae
Ruaraidh <rca.plummer@gmail.com>
2021-02-03 14:02:46 +0000

    #8 Act 4 challenge complete

--- Act4/challenge.js
+++ Act4/challenge.js
@@ -1,2 +1,8 @@
 //cycle over the list items and apply colors from the array called colours
 const colours = ['red', 'yellow', 'goldenrod', 'lightgreen', 'blue', 'purple'];
+
+const listItems = document.querySelectorAll("li");
+
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].style.color = colours[i];
+}
